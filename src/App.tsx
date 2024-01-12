fdc7fc7868b25c36ed2f2b418a4df4d3fd82cfba
Alex Eftink <aeftink@coolfiresolutions.com>
2024-01-12 09:01:57 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import {
   DragOverlay,
   DragStartEvent,
 } from "@dnd-kit/core";
-import { TasksGroups } from "./TasksGroups";
 import { Task } from "./Task";
-import { atom, useAtom, PrimitiveAtom, useSetAtom } from "jotai";
+import { atom, useAtom, PrimitiveAtom } from "jotai";
 import { atomWithImmer } from "jotai-immer";
 import { Virtuoso } from "react-virtuoso";
 import { useDroppable } from "@dnd-kit/core";
+import { faker } from "@faker-js/faker";
 
 enum TaskStatus {
   BACKLOG = "BACKLOG",
@@ -26,10 +26,16 @@ const TaskLabels: { [key: string]: string } = {
   [TaskStatus.COMPLETE]: "Completed 🎉",
 };
 
-type Task = {
+export type Task = {
   id: string;
   title: string;
+  genre: string;
   status: TaskStatus;
+  num: number;
+  first: string;
+  last: string;
+  likes: number;
+  comments: number;
 };
 
 const containers = [
@@ -39,26 +45,85 @@ const containers = [
   TaskStatus.COMPLETE,
 ];
 
-const tasks = new Array(1000).fill(null).map((_, i) => ({
+const genres = new Array(8)
+  .fill(null)
+  .map(() => faker.music.genre())
+  .filter((value, index, arr) => arr.indexOf(value) === index);
+
+// Dummy Data
+const tasks = new Array(100000).fill(null).map((_, i) => ({
   id: crypto.randomUUID(),
-  title: `Task ${i}`,
+  num: i + 1,
+  title: faker.music.songName(),
+  genre: genres[i % genres.length],
   status: TaskStatus.BACKLOG,
+  first: faker.person.firstName(),
+  last: faker.person.lastName(),
+  likes: faker.number.int({ min: 0, max: 100 }),
+  comments: faker.number.int({ min: 0, max: 100 }),
 }));
 
+// Atoms
 const tasksAtom = atomWithImmer<Task[]>(tasks);
+const filterAtom = atom("all");
+const filteredAtom = atom<Task[]>((get) => {
+  const filter = get(filterAtom);
+  const tasks = get(tasksAtom);
+  if (filter === "all") return tasks;
+  return tasks.filter((task) => task.genre === filter);
+});
 
 const taskStatusAtoms: { [key: string]: PrimitiveAtom<Task[]> } =
   containers.reduce((acc, curr) => {
     return {
       ...acc,
       [curr]: atom((get) => {
-        const tasks = get(tasksAtom);
+        const tasks = get(filteredAtom);
         return tasks.filter((task) => task.status === curr);
       }),
     };
   }, {});
 
+// Droppable container for tasks
+function TasksGroups({ taskStatus }: { taskStatus: string }) {
+  const [status] = useAtom(taskStatusAtoms[taskStatus]);
+  const { isOver, setNodeRef } = useDroppable({
+    id: taskStatus,
+  });
+  const style = {
+    color: isOver ? "green" : undefined,
+    height: "100%",
+    width: "100%",
+  };
+
+  return (
+    <div className="col-span-full sm:col-span-6 xl:col-span-3">
+      <div className="flex items-center justify-between mb-2">
+        <h2 className="grow font-semibold text-slate-800 dark:text-slate-100 truncate">
+          {TaskLabels[taskStatus]}
+        </h2>
+        <button className="shrink-0 text-indigo-500 hover:text-indigo-600 dark:hover:text-indigo-400 ml-2">
+          <svg className="w-4 h-4 fill-current" viewBox="0 0 16 16">
+            <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+          </svg>
+        </button>
+      </div>
+      <Virtuoso
+        // @ts-expect-error ref typing
+        scrollerRef={setNodeRef}
+        totalCount={status.length}
+        itemContent={(index) => {
+          const task = status[index];
+          return <Task {...task} />;
+        }}
+        style={style}
+      />
+    </div>
+  );
+}
+
 function App() {
+  const [filter, set] = useAtom(filterAtom);
   const [tasks, updateTasks] = useAtom(tasksAtom);
   const [activeId, setActiveId] = useState<string | null>(null);
   const activeTask = useMemo(() => {
@@ -93,17 +158,71 @@ function App() {
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         <main className="grow">
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto h-full">
+            {/* Page header */}
+            <div className="sm:flex sm:justify-between sm:items-center mb-8">
+              {/* Left: Title */}
+              <div className="mb-4 sm:mb-0">
+                <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold">
+                  Super Massive Kanban Board ✨
+                </h1>
+              </div>
+
+              {/* Right: Actions */}
+              <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+                {/* Add board button */}
+                <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
+                  <svg
+                    className="w-4 h-4 fill-current opacity-50 shrink-0"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                  </svg>
+                  <span className="ml-2">Add Board</span>
+                </button>
+              </div>
+            </div>
+
+            {/* Filters */}
+            <div className="mb-4 border-b border-slate-200 dark:border-slate-700">
+              <ul className="text-sm font-medium flex flex-nowrap -mx-4 sm:-mx-6 lg:-mx-8 overflow-x-scroll no-scrollbar">
+                <li className="pb-3 mr-6 last:mr-0 first:pl-4 sm:first:pl-6 lg:first:pl-8 last:pr-4 sm:last:pr-6 lg:last:pr-8">
+                  <a
+                    onClick={() => set("all")}
+                    className={`${
+                      filter === "all"
+                        ? "text-indigo-500"
+                        : "text-slate-500 dark:text-slate-400 hover:text-slate-600 dark:hover:text-slate-300"
+                    } whitespace-nowrap cursor-pointer`}
+                  >
+                    View All
+                  </a>
+                </li>
+                {genres.map((genre) => {
+                  return (
+                    <li className="pb-3 mr-6 last:mr-0 first:pl-4 sm:first:pl-6 lg:first:pl-8 last:pr-4 sm:last:pr-6 lg:last:pr-8">
+                      <a
+                        onClick={() => set(genre)}
+                        className={`${
+                          filter === genre
+                            ? "text-indigo-500"
+                            : "text-slate-500 dark:text-slate-400 hover:text-slate-600 dark:hover:text-slate-300"
+                        } whitespace-nowrap cursor-pointer`}
+                      >
+                        {genre}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
             <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
               <div className="grid grid-cols-12 gap-x-4 gap-y-8 h-full">
                 {containers.map((status) => (
-                  <TasksGroupsVirtuoso key={status} taskStatus={status} />
-                  // <ContainerGroup key={status} taskStatus={status} />
+                  <TasksGroups key={status} taskStatus={status} />
                 ))}
               </div>
               <DragOverlay>
-                {activeTask ? (
-                  <Task id={activeTask.id} title={activeTask.title} />
-                ) : null}
+                {activeTask ? <Task {...activeTask} /> : null}
               </DragOverlay>
             </DndContext>
           </div>
@@ -114,40 +233,3 @@ function App() {
 }
 
 export default App;
-
-export function TasksGroupsVirtuoso({ taskStatus }: { taskStatus: string }) {
-  const [status] = useAtom(taskStatusAtoms[taskStatus]);
-  const { isOver, setNodeRef } = useDroppable({
-    id: taskStatus,
-  });
-  const style = {
-    color: isOver ? "green" : undefined,
-    height: "100%",
-    width: "100%",
-  };
-
-  return (
-    <div className="col-span-full sm:col-span-6 xl:col-span-3">
-      <div className="flex items-center justify-between mb-2">
-        <h2 className="grow font-semibold text-slate-800 dark:text-slate-100 truncate">
-          {TaskLabels[taskStatus]}
-        </h2>
-        <button className="shrink-0 text-indigo-500 hover:text-indigo-600 dark:hover:text-indigo-400 ml-2">
-          <svg className="w-4 h-4 fill-current" viewBox="0 0 16 16">
-            <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-          </svg>
-        </button>
-      </div>
-      <Virtuoso
-        // @ts-expect-error ref typing
-        scrollerRef={setNodeRef}
-        totalCount={status.length}
-        itemContent={(index) => {
-          const task = status[index];
-          return <Task id={task.id} title={task.title} />;
-        }}
-        style={style}
-      />
-    </div>
-  );
-}
--- src/Task.tsx
+++ src/Task.tsx
@@ -1,6 +1,16 @@
 import { useDraggable } from "@dnd-kit/core";
+import type { Task } from "./App";
 
-export function Task({ id, title }: { id: string; title: string }) {
+export function Task({
+  id,
+  title,
+  num,
+  first,
+  last,
+  genre,
+  likes,
+  comments,
+}: Task) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id,
   });
@@ -21,15 +31,18 @@ export function Task({ id, title }: { id: string; title: string }) {
       {/* Body */}
       <div className="mb-3">
         {/* Title */}
-        <h2 className="font-semibold text-slate-800 dark:text-slate-100 mb-1">
-          {title}
-        </h2>
+        <div className="flex justify-between">
+          <h2 className="font-semibold text-slate-800 dark:text-slate-100 mb-1">
+            {title}
+          </h2>
+          <div>{num}</div>
+        </div>
         {/* Content */}
         <div>
           <div className="text-sm">
-            #7764 created by{" "}
+            {`${genre}, created by `}
             <a className="font-medium text-slate-800 hover:underline" href="#0">
-              markus-james
+              {`${first}-${last}`}
             </a>
           </div>
         </div>
@@ -48,7 +61,9 @@ export function Task({ id, title }: { id: string; title: string }) {
             >
               <path d="M14.682 2.318A4.485 4.485 0 0011.5 1 4.377 4.377 0 008 2.707 4.383 4.383 0 004.5 1a4.5 4.5 0 00-3.182 7.682L8 15l6.682-6.318a4.5 4.5 0 000-6.364zm-1.4 4.933L8 12.247l-5.285-5A2.5 2.5 0 014.5 3c1.437 0 2.312.681 3.5 2.625C9.187 3.681 10.062 3 11.5 3a2.5 2.5 0 011.785 4.251h-.003z" />
             </svg>
-            <div className="text-sm text-slate-500 dark:text-slate-400">4</div>
+            <div className="text-sm text-slate-500 dark:text-slate-400">
+              {likes}
+            </div>
           </button>
           {/* Replies button */}
           <button className="flex items-center text-slate-400 dark:text-slate-500 hover:text-indigo-500  dark:hover:text-indigo-500 ml-3">
@@ -58,7 +73,9 @@ export function Task({ id, title }: { id: string; title: string }) {
             >
               <path d="M8 0C3.6 0 0 3.1 0 7s3.6 7 8 7h.6l5.4 2v-4.4c1.2-1.2 2-2.8 2-4.6 0-3.9-3.6-7-8-7zm4 10.8v2.3L8.9 12H8c-3.3 0-6-2.2-6-5s2.7-5 6-5 6 2.2 6 5c0 2.2-2 3.8-2 3.8z" />
             </svg>
-            <div className="text-sm text-slate-500 dark:text-slate-400">7</div>
+            <div className="text-sm text-slate-500 dark:text-slate-400">
+              {comments}
+            </div>
           </button>
           {/* Attach button */}
           <button className="text-slate-400 dark:text-slate-500 hover:text-indigo-500  dark:hover:text-indigo-500 ml-3">
